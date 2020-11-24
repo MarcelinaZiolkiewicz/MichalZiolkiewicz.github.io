ff33f5977dd5e5529705ace694f8a81e53e3e15e
Michal <mziolkiewicz@edu.cdv.pl>
2020-11-24 19:17:07 +0100

    Dodane pliki lottie i zrobiony pierwszy segment

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('scrollDown'),
+    render: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "./lottie/scrollDown.json",
+});
+
