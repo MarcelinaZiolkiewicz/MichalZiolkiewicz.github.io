c8d155ebdd55827d7a99adc1057e66608e82a758
MichalZiolkiewicz <m.ziolkiewicz1@gmail.com>
2022-01-28 01:01:21 +0100

    Menu update, reorganized files

--- js/main.js
+++ js/main.js
@@ -1,36 +1,5 @@
-// main code
+const chbk = document.getElementById("checkbox");
 
-let pro = document.getElementById("projects");
-let toRight = document.getElementsByClassName("toRight");
-let toLeft = document.getElementsByClassName("toLeft");
-
-var isOnScreen = function (elem) {
-    var bounding = elem.getBoundingClientRect();
-    return (bounding.top + 100 <= (window.innerHeight || document.documentElement.clientHeight));
-};
-
-window.addEventListener('scroll', function (event) {
-    if (isOnScreen(pro)) {
-        console.log("Is on screen");
-
-        for (let i = 0; i < toLeft.length; i++){
-            toLeft[i].classList.add("animate__fadeInLeft");
-            toLeft[i].classList.remove("invisible");
-        }
-
-        for (let j = 0; j < toRight.length; j++){
-            toRight[j].classList.add("animate__fadeInRight");
-            toRight[j].classList.remove("invisible");
-        }
-    } else{
-        for (let i = 0; i < toLeft.length; i++){
-            toLeft[i].classList.remove("animate__fadeInLeft");
-            toLeft[i].classList.add("invisible");
-        }
-
-        for (let j = 0; j < toRight.length; j++){
-            toRight[j].classList.remove("animate__fadeInRight");
-            toRight[j].classList.add("invisible");
-        }
-    }
-}, false);
+const closeMenu = () => {
+    chbk.checked = false;
+}
--- js/script.js
+++ js/script.js
@@ -6,19 +6,4 @@ var animation = bodymovin.loadAnimation({
     path: "./lottie/scrollDown.json",
 });
 
-var coding = bodymovin.loadAnimation({
-    container: document.getElementById('coding'),
-    render: 'svg',
-    loop: true,
-    autoplay: true,
-    path: "./lottie/coding.json",
-});
-
-var plant = bodymovin.loadAnimation({
-    container: document.getElementById('plant'),
-    render: 'svg',
-    loop: true,
-    autoplay: true,
-    path: "./lottie/plant.json",
-});
 
